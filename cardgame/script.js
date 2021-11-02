9b8a5d00d20fd28195bc29e7b9595fbd49a29c9b
codepec <christoph.peschke@gmail.com>
2021-11-02 19:08:19 +0100

    add some js

--- cardgame/script.js
+++ cardgame/script.js
@@ -1,3 +1,66 @@
 let chars = ["snake", "wolves", "tiger", "eagle", "mouse"]
 
-console.log(chars[0])
+monitor = document.querySelector('.monitor');
+box = document.querySelector('.box');
+newGame = document.querySelector('.newGame');
+
+newGame.addEventListener("click", showCards);
+
+
+function showCards(){
+let x = Math.floor(Math.random() * 5);
+  if (x == 0) {
+    console.log(chars[0]);
+    monitor.innerHTML+=chars[0];
+      
+  }
+  else if (x==1){
+    console.log(chars[1]);
+    monitor.innerHTML+=chars[1];
+      
+    }
+
+    else if (x==2){
+      console.log(chars[2]);
+      monitor.innerHTML+=chars[2];
+        
+      }
+
+      else if (x==3){
+        console.log(chars[3]);
+        monitor.innerHTML+=chars[3];
+          
+        }
+
+  else {
+    console.log(chars[4]);
+    monitor.innerHTML+=chars[4];
+  }
+
+  
+
+ 
+   
+  }
+
+
+
+
+
+card1.addEventListener("click", showToggle);
+
+function showToggle(){
+  card1.style.cssText = "animation: attack 1s";
+  cardCPU.style.cssText = "animation: shake 1s;"; 
+  
+
+}
+
+
+
+
+
+
+
+
+//Random I Random II, III, IV, V
