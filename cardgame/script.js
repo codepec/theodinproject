ce67854d9fa5878b05d4cb7df609a863e6c14745
codepec <christoph.peschke@gmail.com>
2021-11-04 12:23:14 +0100

    add some js

--- cardgame/script.js
+++ cardgame/script.js
@@ -4,10 +4,138 @@ monitor = document.querySelector('.monitor');
 box = document.querySelector('.box');
 newGame = document.querySelector('.newGame');
 
-newGame.addEventListener("click", showCards);
+
+// card "snake"
+
+function createSnakeCard(){
 
 
-function showCards(){
+  const snakeCard = document.createElement('div');
+  
+  snakeCard.innerHTML = 'Attack: 2hp <br><br> snake Card <br><br> Life: 2hp' ;
+  snakeCard.className = 'cardBox card1';
+  snakeCard.id = 'card1';
+  snakeCard.style.cssText = "animation: shake 1s;";
+  
+
+
+  monitor.appendChild(snakeCard);
+
+  //<div class="cardBox card1" id="card1">
+  //<p>Life: 1hp</p>
+  //<img src="mouse.jpg" width="60px" />
+  //<p>Attack: 3</p>
+  //</div>
+
+
+}
+
+
+function createtigerCard(){
+
+
+  const tigerCard = document.createElement('div');
+  
+  tigerCard.innerHTML = 'Attack: 2hp <br><br> snake Card <br><br> Life: 2hp' ;
+  tigerCard.className = 'cardBox card1';
+  tigerCard.id = 'card1';
+  tigerCard.style.cssText = "animation: shake 1s;";
+  
+
+
+  monitor.appendChild(tigerCard);
+}
+
+//mix cards
+
+function cardSet() {
+let cardSet = [];
+for (let i = 0; i < 5; i++) {
+  
+  x = Math.floor(Math.random() * 5);
+  cardSet.push(x);
+
+  }
+  return cardSet;
+}
+
+//console.log(cardSet());
+
+
+
+function createDeck(){
+  let deck = [];
+  
+  const arr = cardSet();
+  
+  
+    var length = arr.length;
+    for (var i = 0; i < length; i++) {
+
+    deck.push(chars[arr[i]]);
+
+}
+
+monitor.innerHTML = deck;
+}
+
+
+
+
+mixCards.addEventListener("click", showDeck);
+
+function showDeck(){
+
+  createDeck();
+
+}
+
+
+
+
+
+
+
+
+
+//create HTML card design
+
+
+
+
+
+function addCardDeck() {
+
+  createSnakeCard();
+
+}
+
+
+// attach onclick event handler to add button
+document.getElementById('addCards').addEventListener('click', addCardDeck);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add cards (push method not implemented)
+
+newGame.addEventListener("click", showNewCards);
+
+function showNewCards(){
 let x = Math.floor(Math.random() * 5);
   if (x == 0) {
     console.log(chars[0]);
@@ -37,7 +165,33 @@ let x = Math.floor(Math.random() * 5);
     monitor.innerHTML+=chars[4];
   }
 
+  var div = document.getElementById('hobby');
+
+function addHobby() {
+  var input = document.createElement('input'),
+    button = document.createElement('button');
   
+  input.placeholder = "More hobbies";
+  button.innerHTML = 'X';
+  // attach onlick event handler to remove button
+  button.onclick = removeHobby;
+  
+  div.appendChild(input);
+  div.appendChild(button);
+}
+
+function removeHobby() {
+  // remove this button and its input
+  div.removeChild(this.previousElementSibling);
+  div.removeChild(this);
+}
+
+// attach onclick event handler to add button
+document.getElementById('add').addEventListener('click', addHobby);
+// attach onclick event handler to 1st remove button
+document.getElementById('remove').addEventListener('click', removeHobby);
+
+
 
  
    
