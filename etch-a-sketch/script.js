21814347451d7d574263872cdf9514ad349f4b3a
codepec <christoph.peschke@gmail.com>
2021-12-10 13:43:57 +0100

    add some new stuff

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,42 +5,37 @@ box = document.querySelector(".box");
 newGameButton = document.querySelector(".newGameButton");
 
 
-// create Enemy card deck
-
-const CPUCard = document.createElement("div");
-
-function createDeckCPU() {
-
-  createDeckCPUnew();
-
-  CPUCard.innerHTML = deckCPU[0].name + "<br><br>attack: " + deckCPU[0].attack + "<br>health: " + deckCPU[0].health;
 
 
-    CPUCard.className = "cardBox card4";
-    CPUCard.id = "cardCPU4";
-    CPUCard.style.cssText = "animation: shake 1s; background-color:l" + deckCPU[0].color + ";";
 
-  monitorCPU.appendChild(CPUCard);
 
+function createSquareField() {
+  
+  monitor.innerHTML = "";
+
+  for (let i = 0; i < 16; i++) {
+    
+    createSquare();
+
+  }
+ 
 }
 
 
-// create Enemy card deck
 
-const square = document.createElement("div");
+
 
 function createSquare() {
 
+  const square = document.createElement("div");
 
-  square.innerHTML = "square";
-
-
-    square.className = "squareBox squareX";
-    square.id = "squareX";
+  square.innerHTML = "";
+    square.className = "squareBox square";
+    square.id = "square";
     //square.style.cssText = "animation: shake 1s; background-color:l" + yellow + ";";
 
   monitor.appendChild(square);
 
 }
 
-createSquareButton.addEventListener("click", createSquare);
+createSquareButton.addEventListener("click", createSquareField);
