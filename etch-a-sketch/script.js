13fc65e26a10d878b396add8ddd8e133f2c8e8cd
codepec <christoph.peschke@gmail.com>
2022-01-22 20:17:19 +0100

    javascript kills me

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,8 +3,8 @@
 monitor = document.querySelector(".monitor");
 box = document.querySelector(".box");
 newGameButton = document.querySelector(".newGameButton");
-
-
+square = document.getElementById("square");
+squareBox = document.querySelector(".squareBox");
 
 
 
@@ -15,20 +15,21 @@ function createSquareField() {
 
   for (let i = 0; i < 16; i++) {
     
-    monitor.appendChild(createSquare("square" + i));
-
+    monitor.appendChild(createSquare("square"));//+ i
+    
   }
 
   
 }
 
 
+//let children = monitor.square;  
 
 
 
 function createSquare(idSquare) {
-
-  const square = document.createElement("div");
+  const square = document.createElement("div"); 
+  //const square = document.createElement("div");
 
   square.innerHTML = "";
     square.className = "squareBox";
@@ -36,25 +37,34 @@ function createSquare(idSquare) {
     //square.style.cssText = "animation: shake 1s; background-color:l" + yellow + ";";
 
     
+    return square;
 
-  
-  
-  
 
-  return square;
+}
+
+//children.addEventListener("click", colorSquare);
+
+square.addEventListener("click", colorSquare);
+
+
+function colorSquare(){
+  square.style.cssText = "background-color:yellow;";
 
 }
 
 
+  createSquareButton.addEventListener("click", createSquareField);
+  
+  
+  function clickEventSquare(){
 
-createSquareButton.addEventListener("click", createSquareField);
-
+   }
+  
 
 //square color
-function colorSquare(){
+function colorSquare2(){
   squareBox.style.cssText = "background-color:yellow;";
 
 }
-
 
 
