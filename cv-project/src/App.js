880abfa425604c6555c49089e56f34a8adde3dc7
codepec <christoph.peschke@gmail.com>
2022-11-28 21:10:16 +0100

    update

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,57 +1,23 @@
 import React, { Component } from "react";
 import Overview from "./components/Overview";
 import uniqid from "uniqid";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GeneralInformation from "./components/GeneralInformation";
+import Create from "./components/Create";
+import EducationalExperience from "./components/EducationalExperience";
+import PracticalExperience from "./components/PracticalExperience";
 
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      task: {
-        text: "",
-        id: uniqid(),
-      },
-      tasks: [],
-    };
   }
-
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      },
-    });
-  };
-
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: {
-        text: "",
-        id: uniqid(),
-      },
-    });
-  };
-
   render() {
-    const { task, tasks } = this.state;
-
     return (
-      <div className="create">
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input
-            onChange={this.handleChange}
-            value={task.text}
-            type="text"
-            id="taskInput"
-          />
-
-          <button type="submit">Add Task</button>
-        </form>
-        <Overview tasks={tasks} />
+      <div className="content">
+        <Create />
+        <GeneralInformation />
+        <EducationalExperience />
+        <PracticalExperience />
       </div>
     );
   }
