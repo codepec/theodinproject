bcdd242da7bb664c2c18d35cb60f231bb4ddd4df
codepec <christoph.peschke@gmail.com>
2022-12-01 09:03:35 +0100

    uglify

--- restaurantPage/src/tab2.js
+++ restaurantPage/src/tab2.js
@@ -1,61 +1,31 @@
 console.log("tab2");
 
+export function tab2JS() {
+  const element = document.createElement("div");
 
-    /*Tab 1:
-    description: restaurant
-    hours
-    location
-    */
+  element.innerHTML =
+    "<h1>Pizza Italiano</h1>" +
+    "<h2>Menu</h2>" +
+    '<p class="bold">COLD STARTERS</p>' +
+    "<p>Prawn Cocktail      6.00 $</p>" +
+    "<p>Melon               4.00 $</p>" +
+    '<p class="bold">HOT STARTERS</p>' +
+    "<p>Soup of the day     5.00 $</p>" +
+    "<p>Garlic Mushrooms    5.00 $</p>" +
+    '<p class="bold">FISH DISH</p>' +
+    "<p>Catch of the day   29.50 $</p>" +
+    "<p>Baby Calamari      19.50 $</p>" +
+    "<p>King Prawns        29.50 $</p>" +
+    '<p class="bold">SALADS</p>' +
+    "<p>Green Salad         5.50 $</p>" +
+    "<p>Mixed Salad         6.50 $</p>" +
+    "<p>King Prawns        29.50 $</p>" +
+    '<p class="bold">MAIN DISHES</p>' +
+    '<p class="bold">DRINKS</p>' +
+    '<p class="bold">DESERTS</p>';
 
-    /*Tab 2:
-    menu
-    */
+  element.id = "tab2Content";
+  element.className = "content";
 
-    /*Tab 3:
-    contact us
-    */
-
-
-    export function tab2JS() {
-        const element = document.createElement('div');
-      
-        
-        element.innerHTML = "<h1>Pizza Italiano</h1>" 
-        + "<h2>Menu</h2>"
-        + '<p class="bold">COLD STARTERS</p>'
-        + "<p>Prawn Cocktail      6.00 $</p>"
-        + "<p>Melon               4.00 $</p>"
-
-        + '<p class="bold">HOT STARTERS</p>'
-        + "<p>Soup of the day     5.00 $</p>"
-        + "<p>Garlic Mushrooms    5.00 $</p>"
-
-        + '<p class="bold">FISH DISH</p>'
-        + "<p>Catch of the day   29.50 $</p>"
-        + "<p>Baby Calamari      19.50 $</p>"
-        + "<p>King Prawns        29.50 $</p>"
-
-        + '<p class="bold">SALADS</p>'
-        + "<p>Green Salad         5.50 $</p>"
-        + "<p>Mixed Salad         6.50 $</p>"
-        + "<p>King Prawns        29.50 $</p>"  
-        
-        + '<p class="bold">MAIN DISHES</p>'
-
-        + '<p class="bold">DRINKS</p>'
-      
-
-        + '<p class="bold">DESERTS</p>';
-
-
-
-        element.id = "tab2Content";
-        element.className = "content";
-      
-        return element;
-      }
-
-
-
-
-
+  return element;
+}
--- restaurantPage/src/tab3.js
+++ restaurantPage/src/tab3.js
@@ -1,43 +1,16 @@
 console.log("tab3");
 
+export function tab3JS() {
+  const element = document.createElement("div");
 
-    /*Tab 1:
-    description: restaurant
-    hours
-    location
-    */
+  element.innerHTML =
+    "<h1>Pizza Italiano</h1>" +
+    "<h2>Contact us</h2>" +
+    "<p>548 Middleway Street</p>" +
+    "<p>054 5166 455 666</p>";
 
-    /*Tab 2:
-    menu
-    */
+  element.id = "tab3Content";
+  element.className = "content";
 
-    /*Tab 3:
-    contact us
-    */
-
-    export function tab3JS() {
-        const element = document.createElement('div');
-      
-        
-        element.innerHTML = "<h1>Pizza Italiano</h1>" 
-        
-        + "<h2>Contact us</h2>" 
-        + "<p>548 Middleway Street</p>" 
-        + "<p>054 5166 455 666</p>"
-   
-
-        element.id = "tab3Content";
-        element.className = "content";
-      
-        return element;
-      }
-
-    
-
-
-
-
-
-
-
-
+  return element;
+}
