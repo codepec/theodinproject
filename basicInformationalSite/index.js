5c9a33d84ac5867edfb99bf9cfbc97534ecd17b0
codepec <codepec.de@gmail.com>
2023-09-29 15:53:17 +0200

    update

---
+++ basicInformationalSite/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  // Parse the URL
+  const url = req.url === "/" ? "/index.html" : req.url;
+  const filePath = path.join(__dirname, "public", url);
+  const contentType = getContentType(filePath);
+
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // File not found, send a 404 response
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Not Found</h1>");
+    } else {
+      // Read and serve the file
+      fs.readFile(filePath, (err, content) => {
+        if (err) {
+          // Error reading the file, send a 500 response
+          res.writeHead(500, { "Content-Type": "text/html" });
+          res.end("<h1>500 Internal Server Error</h1>");
+        } else {
+          // Serve the file with the appropriate content type
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(content);
+        }
+      });
+    }
+  });
+});
+
+// Determine the content type based on the file extension
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case ".html":
+      return "text/html";
+    case ".css":
+      return "text/css";
+    case ".js":
+      return "text/javascript";
+    default:
+      return "text/plain";
+  }
+}
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
